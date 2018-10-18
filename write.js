ef7cea1b083e402cfa395f20e2eb8bddf571efbf
DESKTOP-6JRESFM <rlarbgus10@gmail.com>
2018-10-19 04:31:48 +0900

    Release: 0.1.0
    Today meal is?

--- write.js
+++ write.js
@@ -1,6 +1,6 @@
 function write_text(res, text,callback){
     const html_onetwo = ['<html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1">' +
-                         '<title>Today meal menu</title></head><body>', '</body></html>]';
+                         '<title>Today meal menu</title></head><body>', '</body></html>'];
     res.write(html_onetwo[0] + text + html_onetwo[1]);
     return callback();
 }
